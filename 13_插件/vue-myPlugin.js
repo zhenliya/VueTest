af549ba7a7dd5582d52432d50c1614537ecc34a3
zhenliya <zria2009@163.com>
2019-01-31 13:58:20 +0800

    '131'

--- 13_插件/vue-myPlugin.js
+++ 13_插件/vue-myPlugin.js
@@ -1,9 +1,9 @@
 (function (window) {
-  // 定义插件对象
+  // 一.定义插件对象
   const MyPlugin = {}
 
-  // 插件对象中必须包含一个install方法(回调调用)
-  MyPlugin.install = function (Vue, options) {
+  // 二.插件对象中必须包含一个install方法(回调调用)
+  MyPlugin.install = function (Vue, options) { // 加载插件没有执行
     // 1. 添加全局方法或属性
     Vue.myGlobalMethod = function () {
       console.log('Vue函数对象的myGlobalMethod()')
@@ -23,3 +23,5 @@
   // 暴露插件对象
   window.MyPlugin = MyPlugin
 })(window)
+
+//三.声明使用vue 插件，在调用页面声明
